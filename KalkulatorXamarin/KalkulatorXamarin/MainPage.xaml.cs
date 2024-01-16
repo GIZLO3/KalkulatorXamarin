ede1952897fbe181ece18e400d924824450ca809
Michał Giza <m.giza12345@gmail.com>
2024-01-16 18:59:12 +0100

    add Print and OperationToSign methods

--- KalkulatorXamarin/KalkulatorXamarin/MainPage.xaml.cs
+++ KalkulatorXamarin/KalkulatorXamarin/MainPage.xaml.cs
@@ -34,6 +34,64 @@ namespace KalkulatorXamarin
             InitializeComponent();
         }
 
+        private void Print()
+        {
+            if (operation == Operation.None)
+            {
+                outputLabel.Text = a.ToString();
+                outputEquationLabel.Text = "";
+                return;
+            }
+            switch (operation)
+            {
+                case Operation.Add:
+                case Operation.Subtract:
+                case Operation.Multiply:
+                case Operation.Divide:
+                    if (equalsClicked)
+                    {
+                        outputEquationLabel.Text = a.ToString() + " " + OperationToSign(operation) + " " + b.ToString() + " =";
+                        outputLabel.Text = result.ToString();
+                    }
+                    else
+                    {
+                        outputEquationLabel.Text = a.ToString() + " " + OperationToSign(operation);
+                        outputLabel.Text = b.ToString();
+                    }
+                    return;
+                case Operation.Square:
+                    outputEquationLabel.Text = "sqr(" + a.ToString() + ")";
+                    break;
+                case Operation.SquareRoot:
+                    outputEquationLabel.Text = "sqrt(" + a.ToString() + ")";
+                    break;
+                case Operation.Inverse:
+                    outputEquationLabel.Text = "1/" + a.ToString();
+                    break;
+            }
+            outputLabel.Text = result.ToString();
+            a = result;
+            b = 0;
+            result = 0;
+            comma = false;
+            operation = Operation.None;
+        }
+        private string OperationToSign(Operation operation)
+        {
+            switch (operation)
+            {
+                case Operation.Add:
+                    return "+";
+                case Operation.Subtract:
+                    return "-";
+                case Operation.Multiply:
+                    return "x";
+                case Operation.Divide:
+                    return "/";
+            }
+            return "";
+        }
+
         private void OperationClick(object sender, EventArgs e)
         {
 
